3d908b8cb0889f9d6ac17648990ab643035c5f90
vanis <pavlik.vanis@email.cz>
2023-11-24 10:16:33 +0100

    update routes

--- src/(app)/app/not-found.tsx
+++
@@ -1,9 +0,0 @@
-const NotFound = () => {
-  return (
-    <main className="flex items-center justify-center">
-      <div>Hey, here is nothing.</div>
-    </main>
-  );
-};
-
-export default NotFound;
--- src/(app)/app/page.tsx
+++
@@ -1,9 +0,0 @@
-const AppPage = () => {
-  return (
-    <main className="flex items-center justify-center">
-      <div className="text-5xl font-bold">App</div>
-    </main>
-  );
-};
-
-export default AppPage;
